fc32362b5b21e795848192a8b47df887a439bd68
BuildTools <unconfigured@null.spigotmc.org>
2023-12-10 17:11:43 +1100

    - found solution to trebuchet - made code more readable - deleted unnessecary files

--- 1_trebuchet/1_trebuchet.py
+++ 1_trebuchet/1_trebuchet.py
@@ -1,59 +1,19 @@
 def main():
-    # 1abc2 = 12
-    # pqr3stu8vwx = 38
-    # a1b2c3d4e5f = 15
-    # treb7uchet = 77
     
     with open("./1_trebuchet/input.txt", 'r') as file:
         input = file.readlines()
         
     sum = 0
     numbers = ('zero','one','two','three','four','five','six','seven','eight','nine')
-    
-    # Search Every Line
+
     for i in range(len(input)):
         input[i] = input[i].strip("\n")
-        
-        stringNums = []
-        indexOfNums = []
-        
         for k in range(len(numbers)):
             index = input[i].find(numbers[k])
             if index >= 0:
-                stringNums.append(k)
-                indexOfNums.append(index)
+                input[i] = input[i].replace(numbers[k], numbers[k] + str(k) + numbers[k])
         
-        # print(stringNums)
-        # print(indexOfNums)
-        
-        output = ''
-        
-        for j in range(len(input[i])):
-            if j in indexOfNums:
-                posOfNum = indexOfNums.index(j) # 1
-                numberValue = stringNums[posOfNum] # 2
-                # print(numberValue, end='') 
-                output += str(numberValue)
-                
-                # Skip to two len away
-                # if i > 
-                
-                # newPos should be i + len(blah) 
-                # But if newPos is greater than or equal to another item in index besides 'i'
-                # Set i = the value that it is greater than or equal to. 
-                # 
-                
-            else: 
-                # print(input[i][j], end='')
-                output += str(input[i][j])
-
-        
-        print(output)
-        input[i] = output
-        # Changed Sentence
-        # print(input[i])
         a = 0
-        b = 0
         for letter in input[i]:
             if letter.isnumeric() and a == 0:
                 a = int(letter)
--- test2.py
+++
@@ -1,8 +0,0 @@
-i = 0
-while i <= 10:
-    print(i)   
-    if i == 6:
-        print('i is now 6')
-        i += 3
-    i += 1
-        
