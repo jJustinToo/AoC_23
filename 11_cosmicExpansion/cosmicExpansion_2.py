7cb0b4a749cb1c3e06164e43ebd78fe0c0a2a9d8
JustinToo <justintoons13@gmail.com>
2023-12-17 17:10:49 +1100

    solved d11

--- 11_cosmicExpansion/cosmicExpansion_2.py
+++ 11_cosmicExpansion/cosmicExpansion_2.py
@@ -1,4 +1,4 @@
-with open('./11_cosmicExpansion/example.txt', 'r') as file:
+with open('./11_cosmicExpansion/input.txt', 'r') as file:
     input = file.readlines()
 
 def main():
@@ -12,42 +12,29 @@ def main():
         if all(x == "." for x in column):
             emptyCols.append(j)
 
-    # Makes newInput
-    newInput = []
-    for i, line in enumerate(input):
-        text = ""
-        for j, char in enumerate(input[i]):
-            if j in emptyCols:
-                text += char
-                text += char
-            else:
-                text += char
-        text = text.strip("\n")
-        
-        if all(x == "." for x in line.strip("\n")):
-            newInput.append(text)
-            newInput.append(text)
-        else:
-            newInput.append(text)
+    # Finds empty rows indexes
+    emptyRows = []
+    for i, row in enumerate(input):
+        if all(x == "." for x in row.strip("\n")):
+            emptyRows.append(i)
     
     # Finds coords of all "#"
-    
     coords = []
-    for i, line in enumerate(newInput):
-        for j, char in enumerate(newInput[i]):
-            if newInput[i][j] == "#":
+    for i, line in enumerate(input):
+        for j, char in enumerate(input[i]):
+            if input[i][j] == "#":
                 coords.append((i, j))
     
     # Finds distances between all unique coords
     ans = 0
     for i, coord in enumerate(coords):
         for j in range(i, len(coords)):
-            ans += (dist(coord, coords[j]))
+            ans += (dist(coord, coords[j], emptyCols, emptyRows))
         
     print(ans)
     return
 
-def dist(a, b):
+def dist(a, b, emptyCols, emptyRows):
     i1, j1 = a
     i2, j2 = b
 
@@ -57,8 +44,12 @@ def dist(a, b):
     ans = 0
     for i in range(i1, i2):
         ans += 1
+        if i in emptyRows:
+            ans += 1000000 - 1
     for j in range(j1, j2):
         ans += 1
+        if j in emptyCols:
+            ans += 1000000 - 1
 
     return ans
 
